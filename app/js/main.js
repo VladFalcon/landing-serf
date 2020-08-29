d7279e72a3fadbee3286ea260f4c875e2d5ae895
VladFalcon <ZzZ05658@gmail.com>
2020-08-29 17:15:05 +0300

    Created header

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+
+$('.header_slider').slick({
+    infinite: true,
+    // fade: true
+    prevArrow: '<img class="slider-arrows slider-arrows_left" src="img/arrows-left.svg" alt="arrows-right">',
+    nextArrow: '<img class="slider-arrows slider-arrows_right" src="img/arrows-right.svg" alt="arrows-right">',
+    asNavFor : '.slider-dots'
+});
+
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor : '.header-slider'
+});
+});
